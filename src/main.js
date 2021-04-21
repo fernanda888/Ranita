442206552e7d08fc51f262071acf60b4e597ecac
fernanda888 <60996563+fernanda888@users.noreply.github.com>
2021-04-21 16:54:42 -0700

    DONE!!!

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+/*
+Name: Maria Fernanda Becerra
+Title: RocketPatrol Mod. (Ranita)
+Date: 4/21/20
+Time: ~20hrs
+Sources used: https://phaser.io/examples and https://photonstorm.github.io/phaser3-docs/ 
+
+Modifications:
+-Display the time remaining (in seconds) on the screen (10)
+-Implement mouse control for player movement and mouse click to fire (20)
+-Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+*/
 let config = {
     type: Phaser.AUTO,
     width: 640,
